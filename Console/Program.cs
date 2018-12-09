419a7a78dd171cade252812f8545e59d6ae83027
FAAAAT <744879956@qq.com>
2018-12-09 23:04:20 +0800

    add -c options

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,77 +1,104 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using CommandLine;
+﻿using CommandLine;
 using ConsoleApp.LogService;
 using ConsoleApp.Socks;
-using log4net;
-using log4net.Config;
-using log4net.Core;
-using log4net.Repository;
-using log4net.Repository.Hierarchy;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 
 namespace ConsoleApp
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
 
 
 
-			CommandLine.Parser.Default.ParseArguments<Options>(args)
-				.WithParsed(x =>
-				{
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(x =>
+                {
+                    var servers = new List<TransferServer>();
 
-					BaseLogger logger = new BaseLogger(new Log4NetLogger());
+                    if (string.IsNullOrWhiteSpace(x.ConfigFile))
+                    {
+                        BaseLogger logger = new BaseLogger(new Log4NetLogger());
 
-					var listenAddr = x.Listen.Split(':');
-					var proxyAddr = x.Proxy.Split(':');
-					var destAddr = x.Dest.Split(':');
+                        var listenAddr = x.Listen.Split(':');
+                        var proxyAddr = x.Proxy.Split(':');
+                        var destAddr = x.Dest.Split(':');
 
-					var listen = new DnsEndPoint(listenAddr[0], Convert.ToInt32(listenAddr[1]));
-					var proxy = new DnsEndPoint(proxyAddr[0],Convert.ToInt32(proxyAddr[1]));
-					var dest = new DnsEndPoint(destAddr[0],Convert.ToInt32(destAddr[1]));
+                        var listen = new DnsEndPoint(listenAddr[0], Convert.ToInt32(listenAddr[1]));
+                        var proxy = new DnsEndPoint(proxyAddr[0], Convert.ToInt32(proxyAddr[1]));
+                        var dest = new DnsEndPoint(destAddr[0], Convert.ToInt32(destAddr[1]));
 
-					
+                        TransferServer server = new TransferServer(new IPEndPoint(Dns.GetHostEntry(listen.Host).AddressList[0], listen.Port), proxy, dest, logger);
+                        servers.Add(server);
 
-					TransferServer server = new TransferServer(new IPEndPoint(Dns.GetHostEntry(listen.Host).AddressList[0], listen.Port), proxy,dest,logger);
-					
-					while (Console.ReadLine() != "exit")
-					{
-						Console.Write('>');
-					}
-					server.Close();
-				})
-				.WithNotParsed(x =>
-				{
-					foreach (Error error in x)
-					{
-						Console.WriteLine(error);
-					}
-				});
-		}
-	}
+                    }
+                    else
+                    {
+                    }
 
 
-	public class Options
-	{
-		[Option('l', "listen", HelpText = "address:port to listen", Required = true)]
-		public string Listen { get; set; }
+                    while (Console.ReadLine() != "exit")
+                    {
+                        Console.Write('>');
+                    }
 
-		[Option('d', "dest", HelpText = "address:port to where you want to proxy the connection . typically, you can just use localhost or your machine name", Required = true)]
-		public string Dest { get; set; }
+                    foreach (var transferServer in servers)
+                    {
+                        if (transferServer != null)
+                        {
+                            transferServer.Close();
 
-		[Option('p', "proxy", HelpText = "address:port to socks5 proxy")]
-		public string Proxy { get; set; }
+                        }
+                    }
+                })
+                .WithNotParsed(x =>
+                {
+                    foreach (Error error in x)
+                    {
+                        Console.WriteLine(error);
+                    }
+                });
+        }
 
-		[Option('u', "user", HelpText = "user name if is sercured", Required = false)]
-		public string UserName { get; set; }
 
-		[Option('P', "password", HelpText = "password if is sercured", Required = false)]
-		public string Password { get; set; }
-		
-	}
+
+    }
+
+
+    public class Options
+    {
+        [Option('l', "listen", HelpText = "address:port to listen", Required = false)]
+        public string Listen { get; set; }
+
+        [Option('d', "dest", HelpText = "address:port to where you want to proxy the connection . typically, you can just use localhost or your machine name", Required = false)]
+        public string Dest { get; set; }
+
+        [Option('p', "proxy", HelpText = "address:port to socks5 proxy")]
+        public string Proxy { get; set; }
+
+        [Option('u', "user", HelpText = "user name if is sercured", Required = false)]
+        public string UserName { get; set; }
+
+        [Option('P', "password", HelpText = "password if is sercured", Required = false)]
+        public string Password { get; set; }
+
+        [Option('c', "config", HelpText = "Config file path to get the listen port and dest port.", Required = false)]
+        public string ConfigFile { get; set; }
+    }
+
+
+    public class ParameterValidator
+    {
+        public bool ValidateAddr(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            return true;
+        }
+    }
 }
